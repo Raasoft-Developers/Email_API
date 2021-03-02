b4daae59e1789cfa28d1532059add5e386869dda
Abhishek Puranik <Abhishek.Puranik@novigosolutions.com>
2021-03-02 18:09:13 +0530

    Added code for swagger UI

--- Nvg.Api.Email/Startup.cs
+++ Nvg.Api.Email/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
@@ -12,7 +14,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Nvg.Api.Email.AutofacModules;
+using Nvg.Api.Email.Filters;
 using Nvg.Api.Email.Helpers;
 
 namespace Nvg.Api.Email
@@ -35,6 +39,35 @@ namespace Nvg.Api.Email
 
             services.AddEmailService(Program.AppName, Configuration);
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Email API",
+                    Description = "Email API Swagger.",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Placeholder Name",
+                        Email = "Placeholder@gmail.com",
+                        Url = new Uri("https://example.com/user"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+                // Filter to modify what to display in the browser when using swagger.
+                //c.DocumentFilter<OpenApiCustomDocumentFilter>();
+                c.OperationFilter<OpenApiCustomOperationFilter>();
+            });
+
             var container = new ContainerBuilder();
             container.RegisterModule(new ApplicationModule());
             container.Populate(services);
@@ -48,6 +81,15 @@ namespace Nvg.Api.Email
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
 
